f42b4b2e404874cc21f7b5367a07966b0f62bb4c
QYUbit <qulijazi@gmail.com>
2025-04-30 18:56:53 +0200

    basics

---
+++ app/src/main/java/com/qyub/novis/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.qyub.novis
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(onNavigateToDetails: () -> Unit) {
+
+}
---
+++ app/src/main/java/com/qyub/novis/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.qyub.novis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(onNavigateToDetails = { navController.navigate("details") })
+        }
+    }
+}
+
+
+
+@Composable
+fun GestureExample() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .pointerInput(Unit) {
+                detectHorizontalDragGestures(
+                    onDragEnd = { println("Swipe bend") },
+                    onHorizontalDrag = { _, dragAmount ->
+                        println("Horizontals Zither: $dragAmount")
+                    },
+                )
+                detectTapGestures(
+                    onPress = { offset ->
+                        println("Press: $offset")
+                    },
+                )
+            }
+    )
+}
